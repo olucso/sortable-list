e13a6c4bdf74de6de75815af202e0810da99c94f
Lucas <danielpassosdest@gmail.com>
2025-04-28 09:15:43 -0300

    project created + maven resources plugin

---
+++ src/main/java/br/com/lucas/sortablelist/SortablelistApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucas.sortablelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortablelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortablelistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucas/sortablelist/SortablelistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucas.sortablelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortablelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
